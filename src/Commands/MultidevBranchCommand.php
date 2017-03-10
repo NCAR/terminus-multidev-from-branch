16a19e17503581e198bb7af9fa7a7c73a5ce621b
dvance <dvance@ucar.edu>
2017-03-10 11:08:52 -0700

    refactoring!

--- src/Commands/MultidevBranchCommand.php
+++ src/Commands/MultidevBranchCommand.php
@@ -29,17 +29,12 @@ class MultidevBranchCommand extends TerminusCommand implements SiteAwareInterfac
   public function createMultidevFromBranch($site_env, $branch_name, $git_url)
   {
     /**
-     * Make sure the site.env the user wants to use as source exists
      * @var $site Site
      * @var $env Environment
      */
-    list($site, $env) = $this->getOptionalSiteEnv($site_env);
+    list($site, $env) = $this->getSiteAndEnvironment($site_env);
 
-    if(!$site || !$env)
-    {
-      throw new TerminusException("Site {site-env} not found.", ['site-env' => $site_env]);
-    }
-
+    //set a bunch of variables, names, etc used
     list($site_name, ) = $this->parseSiteEnvId($site_env);
 
     // this is also the branch name on pantheon, and pantheon limits to 11 characters
@@ -49,11 +44,13 @@ class MultidevBranchCommand extends TerminusCommand implements SiteAwareInterfac
     $build_dir = sys_get_temp_dir() . "/build-{$branch_name}";
     $clone_dir = $build_dir . '/' . $site->getName();
 
+    $git_dir_options = "--git-dir=$clone_dir/.git --work-tree=$clone_dir/";
+    $git_remote_url = $this->normalizeGitUrl($git_url);
+
     $this->prepareDirectories($build_dir, $clone_dir);
 
-    $environments = $site->getEnvironments();
-
     //always start fresh by deleting the multidev if it already exists
+    $environments = $site->getEnvironments();
     if($environments->has($multi_dev_name))
     {
       $this->deleteEnvironment($environments->get($multi_dev_name));
@@ -67,65 +64,43 @@ class MultidevBranchCommand extends TerminusCommand implements SiteAwareInterfac
       ['multi-dev-site' => $multi_dev_site, 'site-env' => $site_env]
     );
 
+    //create the multidev
     $this->createEnvironment($site, $multi_dev_name, $env);
 
-    //repopulate again...
+    //repopulate site info again, now with new multidev goodness...
     $site = $this->repopulateSiteInfo($site_name);
 
-    $remote_url = $this->normalizeGitUrl($git_url);
-
-    $git_dir_options = "--git-dir=$clone_dir/.git --work-tree=$clone_dir/";
-
-    //get the git clone command for the multidev
-    /** @var Environment $multi_dev_env */
+    //get the git clone command for the new multidev
+    /** @var Environment $multi_dev_env **/
     $multi_dev_env = $site->getEnvironments()->get($multi_dev_name);
-    $info = $multi_dev_env->connectionInfo();
+    $git_command = $this->getGitCommand($multi_dev_env);
 
-    if(!$info['git_command'])
-    {
-      throw new TerminusException("Git info not found for {multi-dev-site}", ['multi-dev-site' => $multi_dev_site]);
-    }
-
-    //clone down the multidev locally
-    $this->log()->notice(
-      "Cloning {multi-dev-site} to {clone-dir}",
-      ['multi-dev-site' => $multi_dev_site, 'clone-dir' => $clone_dir]
+    //clone down the new multidev locally
+    $this->shell(
+      "pushd $build_dir && $git_command && git $git_dir_options checkout -b $multi_dev_name && popd",
+      ["Cloning {multi-dev-site} to {clone-dir}", ['multi-dev-site' => $multi_dev_site, 'clone-dir' => $clone_dir]]
     );
-    $command = "pushd $build_dir && {$info['git_command']} && git $git_dir_options checkout -b $multi_dev_name && popd";
-    $output = shell_exec($command);
-    $this->log()->notice($output);
 
     //pull in the branch from git remote
-    $this->log()->notice("Pulling from git remote...");
-    $command = "git $git_dir_options pull $remote_url $multi_dev_name --no-edit";
-    $output = shell_exec($command);
-    $this->log()->notice($output);
+    $this->shell(
+      "git $git_dir_options pull $git_remote_url $multi_dev_name --no-edit",
+      ["Pulling from git remote..."]
+    );
 
     //set mode to git
     $this->setConnectionMode($multi_dev_env, 'git');
 
-    //commit and push back up to pantheon
-    $this->log()->notice("Pushing up to Pantheon...");
-    $command = "git $git_dir_options push origin $multi_dev_name";
-    $output = shell_exec($command);
-    if($output)
-    {
-      $this->log()->notice($output);
-    }
-    else
-    {
-      $this->log()->notice(
-        "Merged {multi-dev-name} to {multi-dev-site}",
-        ['multi-dev-name' => $multi_dev_name, 'multi-dev-site' => $multi_dev_site]
-      );
-    }
+    //push back up to pantheon
+    $this->shell(
+      "git $git_dir_options push origin $multi_dev_name",
+      ["Pushing up to Pantheon..."]
+    );
 
     //clean up
     $this->deleteDirectory($build_dir);
 
     $url = "http://{$multi_dev_name}-{$site_name}.pantheon.io";
     $this->log()->notice("Done. Multi-dev created at {url}", ['url' => $url]);
-
   }
 
   /**
@@ -273,6 +248,57 @@ class MultidevBranchCommand extends TerminusCommand implements SiteAwareInterfac
       $this->log()->notice("Removing $dir...");
       shell_exec("rm -rf $dir");
     }
+  }
 
+  /**
+   * @param $environment Environment
+   * @return mixed
+   * @throws \Pantheon\Terminus\Exceptions\TerminusException
+   */
+  protected function getGitCommand(Environment $environment)
+  {
+    $info = $environment->gitConnectionInfo();
+
+    if (!$info['command'])
+    {
+      throw new TerminusException("Git info not found for {multi-dev-site}", ['multi-dev-site' => $environment->getSite()->getName() . '.' . $environment->getName()]);
+    }
+
+    return $info['command'];
+  }
+
+  /**
+   * @param $site_env string site-name.env
+   * @return array [Site, Environment]
+   * @throws \Pantheon\Terminus\Exceptions\TerminusException
+   */
+  protected function getSiteAndEnvironment($site_env)
+  {
+    list($site, $env) = $this->getOptionalSiteEnv($site_env);
+
+    if (!$site || !$env)
+    {
+      throw new TerminusException("Site {site-env} not found.", ['site-env' => $site_env]);
+    }
+
+    return [$site, $env];
+  }
+
+  /**
+   * @param $command string
+   * @param array $info_message [message, [params]]
+   * @return string
+   */
+  public function shell($command, array $info_message = ['', []])
+  {
+    if($info_message && isset($info_message[0]))
+    {
+      $this->log()->notice($info_message[0], (isset($info_message[1]) && !empty($info_message[1]) ? $info_message[1] : []));
+    }
+
+    $output = shell_exec($command);
+    if($output) $this->log()->notice($output);
+
+    return $output;
   }
 }
