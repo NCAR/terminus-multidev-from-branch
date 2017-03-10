cf2a0cc5577496da5a900d9b16a7b7a53c49a952
dvance <dvance@ucar.edu>
2017-03-09 18:22:27 -0700

    working implementation of plugin

--- src/Commands/MultidevBranchCommand.php
+++ src/Commands/MultidevBranchCommand.php
@@ -5,6 +5,7 @@ namespace NCAR\TerminusMultidevBranch\Commands;
 use Pantheon\Terminus\Commands\TerminusCommand;
 use Pantheon\Terminus\Models\Environment;
 use Pantheon\Terminus\Models\Site;
+use Pantheon\Terminus\Models\Workflow;
 use Pantheon\Terminus\Site\SiteAwareInterface;
 use Pantheon\Terminus\Site\SiteAwareTrait;
 
@@ -20,12 +21,12 @@ class MultidevBranchCommand extends TerminusCommand implements SiteAwareInterfac
    * @param string $branch_name Git branch name
    * @return bool
    */
-  public function createMultidev($site_env, $branch_name)
+  public function createMultidevFromBranch($site_env, $branch_name)
   {
     /**
-     * Make sure the site.env user wants to use as source exists
+     * Make sure the site.env the user wants to use as source exists
      * @var $site Site
-     * @var $env Environment
+     * @var $multi_dev_env$env Environment
      */
     list($site, $env) = $this->getOptionalSiteEnv($site_env);
 
@@ -37,10 +38,10 @@ class MultidevBranchCommand extends TerminusCommand implements SiteAwareInterfac
 
     // this is also the branch name on pantheon, and pantheon limits to 11 characters
     $multi_dev_name = strtolower(substr($branch_name, 0, 11));
-    $branch = $multi_dev_name;
-
-    $build_dir = "build-$branch_name";
     $multi_dev_site = $site->getName() . '.' . $multi_dev_name;
+
+    $build_dir = sys_get_temp_dir() . "/build-$branch_name";
+    $clone_dir = $build_dir . '/' . $site->getName();
 
     if(!file_exists($build_dir))
     {
@@ -56,20 +57,81 @@ class MultidevBranchCommand extends TerminusCommand implements SiteAwareInterfac
     //create the new multidev if it doesn't exist
     if(!$site->getEnvironments()->has($multi_dev_name))
     {
-      $this->log()->notice("Creating {multidev-site}", ['multidev-site' => $multi_dev_site]);
+      $this->log()->notice(
+        "Creating {multi-dev-site} from {site-env}",
+        ['multi-dev-site' => $multi_dev_site, 'site-env' => $site_env]
+      );
+
+      /** @var Workflow $workflow */
+      $workflow = $site->getEnvironments()->create($multi_dev_name, $env);
+      while (!$workflow->checkProgress())
+      {
+        // @TODO: Add Symfony progress bar to indicate that something is happening.
+      }
+      $this->log()->notice($workflow->getMessage());
+      if(!$workflow->isSuccessful())
+      {
+        $this->log()->error("Failed to create {multi-dev-site}", ['multi-dev-site' => $multi_dev_site]);
+        return false;
+      }
     }
 
-    $git_dir_options = "--git-dir=$build_dir/.git --work-tree=$build_dir/";
+    $git_dir_options = "--git-dir=$clone_dir/.git --work-tree=$clone_dir/";
 
     //get the git clone command for the multidev
+    /** @var Environment $multi_dev_env */
+    $multi_dev_env = $site->getEnvironments()->get($multi_dev_name);
+    $info = $multi_dev_env->connectionInfo();
+
+    if(!$info['git_command'])
+    {
+      $this->log()->error("Git info not found for {multi-dev-site}", ['multi-dev-site' => $multi_dev_site]);
+      return false;
+    }
 
     //clone down the multidev locally
+    $this->log()->notice(
+      "Cloning {multi-dev-site} to {clone-dir}",
+      ['multi-dev-site' => $multi_dev_site, 'clone-dir' => $clone_dir]
+    );
+    $command = "pushd $build_dir && {$info['git_command']} && git $git_dir_options checkout -b $multi_dev_name && popd";
+    $output = shell_exec($command);
+    $this->log()->notice($output);
 
     //pull in the branch from github
+    $command = "git $git_dir_options pull git@github.com:NCAR/pantheon-umbrella-upstream.git $multi_dev_name";
+    $output = shell_exec($command);
+    $this->log()->notice($output);
 
     //set git mode
+    $workflow = $multi_dev_env->changeConnectionMode('git');
+    if(is_string($workflow))
+    {
+      $this->log()->notice($workflow);
+    }
+    else
+    {
+      while (!$workflow->checkProgress())
+      {
+        // TODO: (ajbarry) Add workflow progress output
+      }
+      $this->log()->notice($workflow->getMessage());
+    }
 
     //commit and push back up to pantheon
+    $command = "git $git_dir_options push origin $multi_dev_name";
+    $output = shell_exec($command);
+    if($output)
+    {
+      $this->log()->notice($output);
+    }
+    else
+    {
+      $this->log()->notice(
+        "Merged {multi-dev-name} to {multi-dev-site}",
+        ['multi-dev-name' => $multi_dev_name, 'multi-dev-site' => $multi_dev_site]
+      );
+    }
 
     return true;
   }
